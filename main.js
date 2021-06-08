4629f51184cd106d0375953a3bfae3e4af786049
VulcanCrafter <53003337+VulcanCrafter@users.noreply.github.com>
2021-06-08 17:57:50 +0530

    ....

--- main.js
+++ main.js
@@ -3,16 +3,18 @@
 //pushh
 var checkBox = document.getElementById("check");
 var navigation = document.querySelector(".nav");
+var check=document.querySelector('.checker')
 console.log(navigation)
 console.log(checkBox)
 function navshow(){
   if(checkBox.checked === true){
     navigation.classList.add("checked")
+    check.innerText= "✖"
 
   }
   if(checkBox.checked === false){
     navigation.classList.remove("checked")
-
+    check.innerHTML="&#9776;"
   }
 
 }
